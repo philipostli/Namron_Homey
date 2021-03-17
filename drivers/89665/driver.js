1bd449129e7ccd4ba0c0816d636b4eaab6b10350
maginawin <maginawin@163.com>
2021-03-17 09:30:15 +0800

    Added support for 89665

---
+++ drivers/89665/driver.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const SrZigBeeDriver = require('../../lib/SrZigBeeDriver')
+
+class MyDriver extends SrZigBeeDriver {
+
+  onInit () {
+    super.onInit()
+
+    this._moveHueActionCard = this.getActionCard(
+      '89665_move_hue')
+    this._moveHueActionCard.registerRunListener(async (args, state) => {
+      return args.device.moveHueRunListener(args, state)
+    })
+
+    this._moveSaturationActionCard = this.getActionCard(
+      '89665_move_saturation')
+    this._moveSaturationActionCard.registerRunListener(async (args, state) => {
+      return args.device.moveSaturationRunListener(args, state)
+    })
+
+    this._levelStepActionCard = this.getActionCard('89665_level_step_with_onoff')
+    this._levelStepActionCard.registerRunListener((args, state) => {
+      return args.device.levelStepRunListener(args, state)
+    })
+
+    this._levelMoveActionCard = this.getActionCard('89665_level_move_with_onoff')
+    this._levelMoveActionCard.registerRunListener((args, state) => {
+      return args.device.levelMoveRunListener(args, state)
+    })
+
+    this._levelStopActionCard = this.getActionCard('89665_level_stop_with_onoff')
+    this._levelStopActionCard.registerRunListener((args, state) => {
+      return args.device.levelStopRunListener(args, state)
+    })
+
+    this._stepColorTemperatureActionCard = this.getActionCard(
+      '89665_step_color_temperature')
+    this._stepColorTemperatureActionCard.registerRunListener((args, state) => {
+      return args.device.stepColorTemperatureRunListener(args, state)
+    })
+
+    this._moveColorTemperatureActionCard = this.getActionCard(
+      '89665_move_color_temperature')
+    this._moveColorTemperatureActionCard.registerRunListener((args, state) => {
+      return args.device.moveColorTemperatureRunListener(args, state)
+    })
+
+    this._stopMoveStepActionCard = this.getActionCard(
+      '89665_stop_move_step')
+    this._stopMoveStepActionCard.registerRunListener((args, state) => {
+      return args.device.stopMoveStepRunListener(args, state)
+    })
+
+    this._recallSceneActionCard = this.getActionCard('89665_recall_scene')
+    this._recallSceneActionCard.registerRunListener((args, state) => {
+      return args.device.recallSceneRunListener(args, state)
+    })
+
+    this._storeSceneActionCard = this.getActionCard('89665_store_scene')
+    this._storeSceneActionCard.registerRunListener((args, state) => {
+      return args.device.storeSceneRunListener(args, state)
+    })
+  }
+
+}
+
+module.exports = MyDriver
