d5628fd9aa8367c27a40b1a7c44761447d646f75
maginawin <maginawin@163.com>
2021-08-03 18:37:25 +0800

    Add support for ZV9092A
    Change images/large.png small.png

--- drivers/4512744/device.js
+++ drivers/4512744/device.js
@@ -40,6 +40,10 @@ class MyThermostat extends ZwaveDevice {
     this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL')
 
     this.registerThermostatModeCapability()
+    this.registerTemperatureControlReferenceSelectionCapability()
+
+    this.registerTemperatureSensorCapability('my_ZV9092A_home_sensor', 1)
+    this.registerTemperatureSensorCapability('my_ZV9092A_floor_sensor', 2)
   }
 
   registerThermostatModeCapability () {
@@ -98,7 +102,8 @@ class MyThermostat extends ZwaveDevice {
             const capabilityMode = ThermostatModeToCapability[mode]
             this.log('Capability Mode ', capabilityMode)
 
-            if (CapabilityToThermostatSetpointType.hasOwnProperty(capabilityMode)) {
+            if (CapabilityToThermostatSetpointType.hasOwnProperty(
+              capabilityMode)) {
 
               this.thermostatSetpointType = CapabilityToThermostatSetpointType[capabilityMode]
 
@@ -118,6 +123,132 @@ class MyThermostat extends ZwaveDevice {
         return null
       },
     })
+  }
+
+  registerTemperatureControlReferenceSelectionCapability () {
+
+    this.registerCapability('my_ZV9092A_temperature_control_reference',
+      'CONFIGURATION', {
+        get: 'CONFIGURATION_GET',
+        getOpts: {
+          getOnStart: true,
+        },
+        getParser: (value) => {
+
+          this.log('CONFIGURATION_GET ', value)
+
+          return {
+            'Parameter Number': 10,
+          }
+        },
+        set: 'CONFIGURATION_SET',
+        setParser: (value) => {
+
+          this.log('CONFIGURATION_SET ', value)
+
+          let selection = 1
+
+          if (value === 'room_sensor') {
+
+            selection = 1
+
+          } else if (value === 'floor_sensor') {
+
+            selection = 2
+
+          } else if (value === 'room_floor_sensor') {
+
+            selection = 3
+          }
+
+          return {
+            'Parameter Number': 10,
+            'Level': {
+              'Size': 1,
+              'Default': false,
+            },
+            'Configuration Value': Buffer.from([selection]),
+          }
+
+        },
+        report: 'CONFIGURATION_REPORT',
+        reportParser: (value) => {
+
+          this.log('CONFIGURATION_REPORT ', value)
+
+          if (value
+            && value.hasOwnProperty('Parameter Number')
+            && value['Parameter Number'] === 10
+            && value.hasOwnProperty('Configuration Value')) {
+
+            const configurationValue = value['Configuration Value']
+            if (typeof configurationValue === 'object' &&
+              Buffer.isBuffer(configurationValue)) {
+
+              const currentValue = configurationValue[0]
+              this.log('configurationValue ', currentValue)
+
+              if (currentValue === 1) {
+
+                // this.setCapabilityValue('my_ZV9092A_temperature_control_reference', 'room_sensor').then()
+
+                return 'room_sensor'
+
+              } else if (currentValue === 2) {
+
+                // this.setCapabilityValue('my_ZV9092A_temperature_control_reference', 'floor_sensor').then()
+
+                return 'floor_sensor'
+
+              } else if (currentValue === 3) {
+
+                // this.setCapabilityValue('my_ZV9092A_temperature_control_reference', 'room_floor_sensor').then()
+
+                return 'room_floor_sensor'
+              }
+            }
+          }
+        },
+      })
+  }
+
+  registerTemperatureSensorCapability (capabilityId, nodeId) {
+
+    // my_ZV9092A_floor_sensor
+    this.registerCapability(capabilityId, 'SENSOR_MULTILEVEL',
+      {
+        multiChannelNodeId: nodeId,
+        get: 'SENSOR_MULTILEVEL_GET',
+        getOpts: {
+          getOnOnline: true,
+          getOnStart: true,
+        },
+        getParser: () => ({
+          'Sensor Type': 'Temperature (version 1)',
+          Properties1: {
+            Scale: 0,
+          },
+        }),
+        report: 'SENSOR_MULTILEVEL_REPORT',
+        reportParser: report => {
+          if (
+            report
+            && report.hasOwnProperty('Sensor Type')
+            && report['Sensor Type'] === 'Temperature (version 1)'
+            && report.hasOwnProperty('Sensor Value (Parsed)')
+            && report.hasOwnProperty('Level')
+            && report.Level.hasOwnProperty('Scale')
+          ) {
+            // Some devices send this when no temperature sensor is connected
+            if (report['Sensor Value (Parsed)'] === -999.9) return null
+            if (report.Level.Scale ===
+              0) return report['Sensor Value (Parsed)']
+            if (report.Level.Scale ===
+              1) return (report['Sensor Value (Parsed)'] - 32) / 1.8
+          }
+          return null
+        },
+      })
   }
 
 }
