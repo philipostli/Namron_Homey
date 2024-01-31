20dd3b8cb47059cf6adbf1fb7e9741739df07219
yjh <13556200435@163.com>
2024-01-31 10:06:30 +0800

    pull code

--- drivers/451275_X/lib/devices/thermostat_regulator_mode.js
+++ drivers/451275_X/lib/devices/thermostat_regulator_mode.js
@@ -1,73 +1,72 @@
 'use strict'
-const { CLUSTER, Cluster } = require('zigbee-clusters')
-const { 
-  TIP_CHANGED
-}                               = require('./utils');
-module.exports = {  
-  
-  async setConfig(device, payload){ 
-    console.log('regulator Mode SET:', payload); 
-    
-    if (payload === false || payload === '0'){   
-      
-      //不启用
-      
-      const settings = device.getSettings();
-      let mode = settings.sensor_mode; 
-      if (mode === 'p'){
-        mode = 'a'; 
-      } 
+const {
+    TIP_CHANGED
+} = require('./utils');
+module.exports = {
 
-      let payload2 = {}
-      //sensor_mode !p
-      payload2['sensorMode'] = mode; 
-      //regulator set 0(off)
-      payload2['regulator'] = 0;
+    async setConfig(device, payload) {
+        console.log('regulator Mode SET:', payload);
 
-      device.thermostatCluster().writeAttributes(payload2).catch(this.error)
+        if (payload === false || payload === '0') {
 
-    } else if (payload === true || payload === '1'){
+            //不启用
 
-      if (device.hasCapability('t7e_zg_thermostat_mode')){
-        let cur_thermostat = device.getCapabilityValue('t7e_zg_thermostat_mode');
-        if (cur_thermostat === 'cool'){  
-          await device.setWarning("In cooling mode, the regulator heating cannot be switched.").catch(this.error);
-          return
+            const settings = device.getSettings();
+            let mode = settings.sensor_mode;
+            if (mode === 'p') {
+                mode = 'a';
+            }
+
+            let payload2 = {}
+            //sensor_mode !p
+            payload2['sensorMode'] = mode;
+            //regulator set 0(off)
+            payload2['regulator'] = 0;
+
+            device.thermostatCluster().writeAttributes(payload2).catch(this.error)
+
+        } else if (payload === true || payload === '1') {
+
+            if (device.hasCapability('t7e_zg_thermostat_mode')) {
+                let cur_thermostat = device.getCapabilityValue('t7e_zg_thermostat_mode');
+                if (cur_thermostat === 'cool') {
+                    await device.setWarning("In cooling mode, the regulator heating cannot be switched.").catch(this.error);
+                    return
+                }
+            }
+
+            //set p mode: 移到切换中设置
+            //device.setSettings({
+            //  sensor_mode: 'p',
+            //});
+
+            const settings = device.getSettings();
+            let reg = settings.regulator;
+            let num = reg.replace('min', '');
+            num = num.trim();
+            if (num === 'OFF') {
+                num = '0';
+            }
+
+
+            let payload2 = {}
+            //sensor_mode
+            payload2['sensorMode'] = 'p'
+
+            //regulator set min
+            payload2['regulator'] = parseInt(num);
+
+            device.thermostatCluster().writeAttributes(payload2).then(() => {
+                device.driver.triggerRegulator(device)
+            }).catch(this.error)
+
         }
-      } 
 
-      //set p mode: 移到切换中设置
-      //device.setSettings({ 
-      //  sensor_mode: 'p',
-      //}); 
-
-      const settings = device.getSettings();   
-      let reg = settings.regulator;
-      let num = reg.replace('min', '');
-      num = num.trim();
-      if (num === 'OFF') {
-        num = '0';
-      }
+        device.setStoreValue('regulator_mode', payload);
+        device.setStoreValue('regulator_mode_changed', true);
+        await device.showMessage(TIP_CHANGED);
 
 
-      let payload2 = {}
-      //sensor_mode
-      payload2['sensorMode'] = 'p' 
-      
-      //regulator set min
-      payload2['regulator'] = parseInt(num); 
-
-      device.thermostatCluster().writeAttributes(payload2).then(() => {
-        device.driver.triggerRegulator(device)
-      }).catch(this.error)
-
-    }
-    
-    device.setStoreValue('regulator_mode', payload);
-    device.setStoreValue('regulator_mode_changed', true);
-    await device.showMessage(TIP_CHANGED);
-   
-
-  },
+    },
 
 }  
